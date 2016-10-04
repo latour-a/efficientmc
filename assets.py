09d603ce898982ce9e2f96b05c5a1101e093defe
latour-a <latour-a@users.noreply.github.com>
2016-10-04 08:03:55 +0200

    [Actifs] Mise à jour des docstrings.

--- assets.py
+++ assets.py
@@ -3,13 +3,25 @@ import scipy.stats as sps
 from abc import ABCMeta, abstractmethod
 
 class Asset(metaclass=ABCMeta):
-    """
-    TODO
-    """
+    "Actif financier à valoriser et à couvrir."
 
     def getprice(self, date, price, model, analytical=False, nsims=None):
         """
-        TODO
+        Calcule le prix de l'actif.
+
+        Paramètres
+        -----------
+        date
+            Date à laquelle calculer le prix de l'actif.
+        price
+            Valeur des sous-jacents à la date `date`.
+        model
+            Modèle de prix utilisé pour les sous-jacents.
+        analytical : booléen, `False` par défaut
+            Utilise une formule fermée pour effectuer le calcul.
+        nsims : entier ou `None`
+            Nombre de simulations à utiliser dans le cadre d'un calcul
+            Monte Carlo.
         """
         if analytical:
             modelname = type(model).__name__
@@ -21,11 +33,6 @@ class Asset(metaclass=ABCMeta):
                                           (type(self).__name__, modelname))
             return func(date, price, model)
         else: # Monte Carlo
-            #TODO: le nsims n'est pas clair.
-            #TODO: gérer la pondération (dans le cas de l'importance sampling).
-            #TODO: tel quel, absurde de pouvoir donner un tableau (de manière
-            # sous-jacente, on suppose quand même qu'ils partent tous d'une
-            # même valeur initiale).
             if not np.array(price).shape and nsims > 0:
                 price = price + np.zeros((nsims))
             values = self.computepayoff(date, price, model)
@@ -33,15 +40,33 @@ class Asset(metaclass=ABCMeta):
 
     def getdelta(self, date, price, model, analytical=False, simulator=None):
         """
-        TODO
+        Calcule le delta de l'actif.
+
+        Paramètres
+        -----------
+        date
+            Date à laquelle calculer le delta de l'actif.
+        price
+            Valeur des sous-jacents à la date `date`.
+        model
+            Modèle de prix utilisé pour les sous-jacents.
+        analytical : booléen, `False` par défaut
+            Utilise une formule fermée pour effectuer le calcul.
+        nsims : entier ou `None`
+            Nombre de simulations à utiliser dans le cadre d'un calcul
+            Monte Carlo.
         """
         if analytical:
-            raise NotImplementedError("TODO")
+            modelname = type(model).__name__
+            try:
+                func = getattr(self, "_get_" + modelname + "_delta")
+            except AttributeError:
+                raise NotImplementedError("no analytical method implemented "\
+                                          "for asset %s with model %s." % \
+                                          (type(self).__name__, modelname))
+            return func(date, price, model)
         else: # Monte Carlo
-            if not simulator:
-                simulator = None # Simulateur par défaut.
-            #TODO: tenir compte des taux.
-            raise NotImplementedError("TODO")
+            raise NotImplementedError
 
     @abstractmethod
     def computepayoff(self, date, prices, model):
@@ -61,9 +86,7 @@ class Asset(metaclass=ABCMeta):
         pass
 
 class EuropeanCall(Asset):
-    """
-    TODO
-    """
+    "Call européen, option de payoff :math:`(S_T - K)^+`."
 
     def __init__(self, strike, maturity):
         """
@@ -73,15 +96,13 @@ class EuropeanCall(Asset):
         ----------
         strike : double
             Strike de l'option.
-        maturity : double
+        maturity
             Maturité de l'option (i.e. : la date d'exercice est `maturity`).
         """
         self.strike = strike
         self.maturity = maturity
 
     def computepayoff(self, date, price, model):
-        #TODO: on est en train de mélanger Monte Carlo et calcul du payoff.
-        #TODO: faire un décorateur pour la vérification des dates...?
         if date <= self.maturity:
             df = model.getdf(date, self.maturity)
             newprices = model.simulate(date, price, self.maturity)
@@ -91,12 +112,18 @@ class EuropeanCall(Asset):
 
     def _get_BlackScholes_d1(self, date, price, model):
         """
-        TODO
+        Calcule le terme :math:`d_1` intervenant dans la formule de
+        Black-Scholes.
+
+        Paramètres
+        -----------
+        date
+            Date à laquelle calculer :math:`d_1`.
+        price : double ou np.array
+            Valeur du sous-jacent à la date `date`.
+        model
+            Modèle de prix utilisé pour le sous-jacent.
         """
-        #TODO: en l'occurrence, on pourrait faire une sous-classe qui hérite
-        # de EuropeanCall, qui contient les bouts de code spécifiques à
-        # Black-Scholes, et qui est instanciée à la volée par price : permet
-        # de davantage séparer les calculs pour les différents modèles.
         ttm = self.maturity - date
         d1 = (np.log(price / self.strike) + (0.5 * model.sigma**2 + model.rate) * ttm) \
              / (model.sigma * np.sqrt(ttm))
@@ -104,7 +131,16 @@ class EuropeanCall(Asset):
 
     def _get_BlackScholes_price(self, date, price, model):
         """
-        TODO
+        Calcule le prix de l'option dans le cadre du modèle de Black-Scholes.
+
+        Paramètres
+        -----------
+        date
+            Date à laquelle calculer le prix de l'option.
+        price : double ou np.array
+            Valeur du sous-jacent à la date `date`.
+        model
+            Modèle de prix utilisé pour le sous-jacent.
         """
         if date < self.maturity:
             d1 = self._get_BlackScholes_d1(date, price, model)
@@ -112,14 +148,22 @@ class EuropeanCall(Asset):
             df = model.getdf(date, self.maturity)
             return price * sps.norm.cdf(d1) - self.strike * df * sps.norm.cdf(d2)
         elif date == self.maturity:
-            #TODO: bancal.
             return self.computepayoff(date, price, model)
         else:
             return 0.
 
-    def _get_BlackScholes_delta(self, date, price):
+    def _get_BlackScholes_delta(self, date, price, model):
         """
-        TODO
+        Calcule le delta de l'option dans le cadre du modèle de Black-Scholes.
+
+        Paramètres
+        -----------
+        date
+            Date à laquelle calculer le delta.
+        price : double ou np.array
+            Valeur du sous-jacent à la date `date`.
+        model
+            Modèle de prix utilisé pour le sous-jacent.
         """
         if date < self.maturity:
             d1 = self._get_BlackScholes_d1(date, price, model)

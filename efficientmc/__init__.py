7cf6f2386c9c5f4e9fb53c746a41f6c64e237152
latour-a <latour-a@users.noreply.github.com>
2016-11-10 19:29:07 +0100

    [Delta] Calcul des deltas.

--- efficientmc/__init__.py
+++ efficientmc/__init__.py
@@ -15,10 +15,13 @@ def runmc(*allassets):
     Calcule par simulation Monte-Carlo les cash-flows des actifs de
     `allassets`, sous la probabilité risque-neutre.
     """
-    res = {asset.name: {} for asset in allassets}
     allmarkets = set([m for asset in allassets for m in asset.getmarkets()])
     marketsdates = set([d for market in allmarkets for d in market.getdates()])
     assetsdates = set([d for asset in allassets for d in asset.getdates()])
+    earnings = {asset.name: {} for asset in allassets}
+    prices = {market.name: {} for market in allmarkets}
+    volumes = {asset.name: {m.name: {} for m in asset.getmarkets()} \
+               for asset in allassets}
     alldates = assetsdates.union(marketsdates)
     for date in alldates:
         #FIXME: utiliser une notion de `computation` pour sauvegarder et
@@ -28,23 +31,80 @@ def runmc(*allassets):
             market.simulate(date)
         # Mise à jour des objets :
         for asset in allassets:
-            val = asset.get_discounted_cf(date)
-            res[asset.name][date] = val
-    return pd.Panel(res).transpose(1, 0, 2)
+            earnings[asset.name][date] = asset.get_discounted_cf(date)
+            for market in asset.getmarkets():
+                volumes[asset.name][market.name][date] = asset.getvolume(date,
+                                                                         market)
+                #FIXME: on peut avoir besoin d'autre chose que du spot.
+                if date not in prices[market.name]:
+                    prices[market.name][date] = market.getspot(date)
+    earnings = pd.Panel(earnings).transpose(1, 0, 2)
+    #FIXME: utiliser xarray.
+    volumes = {key: pd.Panel(value).transpose(1, 0, 2)
+               for key, value in volumes.items()}
+    prices = pd.Panel(prices).transpose(1, 0, 2)
+    return earnings, volumes, prices
 
-def postpro(sims, alpha=0.95):
+def getmtm(cf, alpha=0.95):
     """
-    Calcule la moyenne des cash-flows (et l'intervalle de confiance associé)
-    pour chaque actif simulé dans `sims`.
+    Calcule la MtM (i.e. : les cash-flows moyens réalisés et un intervalle
+    de confiance) de chaque actif listé dans `cf`.
+
+    Paramètres
+    ----------
+    cf : pandas.Panel
+        Cash-flows réalisés pour chaque simulation (`items`), chaque
+        actif (`major_axis`) et chaque date (`minor_axis`).
+    alpha : double compris entre 0. et 1.
+        Quantile à utiliser pour le calcul des intervalles de confiances.
     """
-    nsims = sims.shape[0]
-    cumvalues = sims.sum(axis=2)
+    nsims = cf.shape[0]
+    cumvalues = cf.sum(axis=2)
     mean = cumvalues.mean(axis=1)
     std = cumvalues.std(axis=1)
-    coeff = sps.norm.ppf(0.5 + 0.5 * alpha)
     res = {}
     for key, val in mean.items():
-        low = val - coeff * std[key] / np.sqrt(nsims)
-        up = val + coeff * std[key] / np.sqrt(nsims)
-        res[key] = MCResults(val, low, up)
+        res[key] = mkmcresults(val, std[key], nsims)
     return res
+
+def getdelta(volumes, prices, alpha=0.95):
+    """
+    Calcule les deltas (avec intervalles de confiance) de chaque actif
+    listé dans `cf`.
+
+    Paramètres
+    ----------
+    cf : pandas.Panel
+        Cash-flows réalisés pour chaque simulation (`items`), chaque
+        actif (`major_axis`) et chaque date (`minor_axis`).
+    volumes : dictionnaire de pandas.Panel
+        Volumes exercés pour chaque actif (clef du dictionnaire), chaque
+        simulation (`items`), chaque marché (`major_axis`) et chaque
+        date (`minor_axis`).
+    prices : pandas.Panel
+        Prix réalisés pour chaque simulation (`items`), chaque marché
+        (`major_axis`) et chaque date (`minor_axis`).
+    alpha : double compris entre 0. et 1.
+        Quantile à utiliser pour le calcul des intervalles de confiances.
+    """
+    nsims = prices.shape[0]
+    res = {}
+    for asset, volume in volumes.items():
+        for market in volume.major_axis:
+            #FIXME: laisser le choix du niveau d'agrégation du delta.
+            price = prices.loc[:, market, :]
+            initfwd = price.mean(axis=1)
+            tmp = (volume.loc[:, market, :] * price).T / initfwd
+            delta = (tmp * initfwd) / initfwd.sum()
+            res[(asset, market)] = mkmcresults(delta.mean(), delta.std(), nsims)
+    return res
+
+def mkmcresults(mean, std, nsims, alpha=0.95):
+    """
+    Crée un objet `MCResults` contenant `mean` et l'intervalle de
+    confiance associé.
+    """
+    coeff = sps.norm.ppf(0.5 + 0.5 * alpha)
+    low = mean - coeff * std / np.sqrt(nsims)
+    up = mean + coeff * std / np.sqrt(nsims)
+    return MCResults(mean, low, up)
--- test.py
+++ test.py
@@ -11,9 +11,14 @@ market = mc.pricemodels.BlackScholesModel("BlackScholes", 100., 0., 0.2, gen)
 itm = mc.assets.EuropeanCall("itm", market, strike=90., maturity=1.)
 atm = mc.assets.EuropeanCall("atm", market, strike=100., maturity=1.)
 otm = mc.assets.EuropeanCall("otm", market, strike=110., maturity=1.)
-sims = mc.runmc(otm, atm, itm)
-res = mc.postpro(sims)
-for k, v in res.items():
+cf, volumes, prices = mc.runmc(otm, atm, itm)
+mtm = mc.getmtm(cf)
+print("Classique - MtM")
+for k, v in mtm.items():
+    print("%s: %.5f [%.5f, %.5f]" % (k, v.mean, v.iclow, v.icup))
+delta = mc.getdelta(volumes, prices)
+print("Classique - Delta")
+for k, v in delta.items():
     print("%s: %.5f [%.5f, %.5f]" % (k, v.mean, v.iclow, v.icup))
 
 # Monte Carlo antithétique :
@@ -23,7 +28,12 @@ market = mc.pricemodels.BlackScholesModel("BlackScholes", 100., 0., 0.2, gen)
 itm = mc.assets.EuropeanCall("itm", market, strike=90., maturity=1.)
 atm = mc.assets.EuropeanCall("atm", market, strike=100., maturity=1.)
 otm = mc.assets.EuropeanCall("otm", market, strike=110., maturity=1.)
-sims = mc.runmc(otm, atm, itm)
-res = mc.postpro(sims)
-for k, v in res.items():
+cf, volumes, prices = mc.runmc(otm, atm, itm)
+mtm = mc.getmtm(cf)
+print("\nAntithétique - Mtm:")
+for k, v in mtm.items():
+    print("%s: %.5f [%.5f, %.5f]" % (k, v.mean, v.iclow, v.icup))
+delta = mc.getdelta(volumes, prices)
+print("Antithétique - Delta")
+for k, v in delta.items():
     print("%s: %.5f [%.5f, %.5f]" % (k, v.mean, v.iclow, v.icup))

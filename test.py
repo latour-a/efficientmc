beae4437a59f97587d7d9256bf7784395028c252
latour-a <latour-a@users.noreply.github.com>
2017-02-08 16:01:22 +0100

    [Tests] Ajout de tests pour l'option sur spread.

--- test.py
+++ test.py
@@ -2,26 +2,27 @@ import numpy as np
 import efficientmc as mc
 from functools import partial
 
-def runtests(allgenerators, partialmarket, partialassets):
+def runtests(allgenerators, partialmarkets, partialassets):
     """
-    Calcule la MtM et le delta de tous les actifs de `partialassets` en
-    leur associant le marché `partialmarket` et en utilisant successivement
-    les générateurs Monte Carlo de `allgenerators`.
+    Calcule la MtM et le delta de tous les actifs de `partialassets`
+    en leur associant les marchés `partialmarkets` et en utilisant
+    successivement les générateurs Monte Carlo de `allgenerators`.
 
     Paramètres :
     ------------
     allgenerators
         Liste de générateurs Monte Carlo.
-    partialmarket
-        Constructeur permettant d'instancier un marché dont tous les
-        paramètres ont été fixés hormis le générateur Monte Carlo.
+    partialmarkets
+        Liste de constructeur permettant d'instancier des marchés dont
+        tous les paramètres ont été fixés hormis le générateur Monte
+        Carlo.
     allassets
         Liste de constructeurs permettant d'instancier des actifs dont
         tous les paramètres ont été fixés hormis le marché.
     """
     for key, gen in allgenerators.items():
-        market = partialmarket(gen)
-        allassets = [asset(market=market) for asset in partialassets]
+        allmarkets = {key: mkt(gen) for key, mkt in partialmarkets.items()}
+        allassets = [asset(**allmarkets) for asset in partialassets]
         cf, volumes, prices = mc.runmc(*allassets)
         mtm = mc.getmtm(cf)
         print("%s - MtM" % key)
@@ -44,8 +45,20 @@ if __name__ == '__main__':
                      "Van Der Corput": mc.generators.GaussianGenerator(5000, np.array([[1.]]), ["BlackScholes"], mc.generators.van_der_corput_dimension),
                      "Halton": mc.generators.GaussianGenerator(500, np.array([[1.]]), ["BlackScholes"], mc.generators.halton),
                      "Halton 2": mc.generators.GaussianGenerator(50000, np.array([[1.]]), ["BlackScholes"], mc.generators.halton2)}
-    PARTIALMARKET = partial(mc.pricemodels.BlackScholesModel, "BlackScholes", 100., 0., 0.2)
+    PARTIALMARKETS = {"market": partial(mc.pricemodels.BlackScholesModel, "BlackScholes", 100., 0., 0.2)}
     PARTIALASSETS = [partial(mc.assets.EuropeanCall, name="itm", strike=90., maturity=1.),
                      partial(mc.assets.EuropeanCall, name="atm", strike=100., maturity=1.),
                      partial(mc.assets.EuropeanCall, name="otm", strike=110., maturity=1.),]
-    runtests(ALLGENERATORS, PARTIALMARKET, PARTIALASSETS)
+    runtests(ALLGENERATORS, PARTIALMARKETS, PARTIALASSETS)
+
+    # Spreads européens, modèle de Black-Scholes :
+    ALLGENERATORS = {"Classique": mc.generators.GaussianGenerator(500000, np.array([[1.0, 0.5], [0.5, 1.0]]), ["BlackScholes1", "BlackScholes2"], np.random.randn),
+                     "Antithétique": mc.generators.GaussianGenerator(500000, np.array([[1.0, 0.5], [0.5, 1.0]]), ["BlackScholes1", "BlackScholes2"], mc.generators.antithetic_randn),
+                     "Sobol": mc.generators.GaussianGenerator(50000, np.array([[1.0, 0.5], [0.5, 1.0]]), ["BlackScholes1", "BlackScholes2"], mc.generators.sobol),
+                     "Van Der Corput": mc.generators.GaussianGenerator(5000, np.array([[1.0, 0.5], [0.5, 1.0]]), ["BlackScholes1", "BlackScholes2"], mc.generators.van_der_corput_dimension),
+                     "Halton": mc.generators.GaussianGenerator(500, np.array([[1.0, 0.5], [0.5, 1.0]]), ["BlackScholes1", "BlackScholes2"], mc.generators.halton),
+                     "Halton 2": mc.generators.GaussianGenerator(50000, np.array([[1.0, 0.5], [0.5, 1.0]]), ["BlackScholes1", "BlackScholes2"], mc.generators.halton2)}
+    PARTIALMARKETS = {"market1": partial(mc.pricemodels.BlackScholesModel, "BlackScholes1", 100., 0., 0.2),
+                      "market2": partial(mc.pricemodels.BlackScholesModel, "BlackScholes2", 100., 0., 0.2)}
+    PARTIALASSETS = [partial(mc.assets.EuropeanSpread, name="spread", maturity=1.)]
+    runtests(ALLGENERATORS, PARTIALMARKETS, PARTIALASSETS)
